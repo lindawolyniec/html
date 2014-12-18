6b9001c91a69b075a16f71b53c819f0bf63a6bfc
lindawolyniec <lindawolyniec@hotmail.com>
2014-12-18 09:30:52 -0500

    website

---
+++ qofa/main.js
@@ -0,0 +1,46 @@
+
+    // $('.carousel').carousel({
+    //     interval: 3000
+    // })
+
+
+
+
+
+
+
+
+
+
+/*$(document).ready(function(){
+$('.jcarousel')
+    .on('jcarousel:create jcarousel:reload', function() {
+        var element = $(this),
+            width = element.innerWidth();
+
+        // This shows 1 item at a time.
+        // Divide `width` to the number of items you want to display,
+        // eg. `width = width / 3` to display 3 items at a time.
+        element.jcarousel('items').css('width', width + 'px');
+    })
+    .jcarousel({
+        // Your configurations options
+    });
+    $('.jcarousel')
+    .on('jcarousel:create jcarousel:reload', function() {
+        var element = $(this),
+            width = element.innerWidth();
+
+        if (width > 900) {
+            width = width / 3;
+        } else if (width > 600) {
+            width = width / 2;
+        }
+
+        element.jcarousel('items').css('width', width + 'px');
+    })
+    .jcarousel({
+        // Your configurations options
+    });
+	
+});*/
